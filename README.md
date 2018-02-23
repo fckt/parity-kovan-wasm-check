81799d31818860253a03a5a927c0dd493b180753
fro <frol.rage@gmail.com>
2018-02-23 15:57:55 +0300

    fixes

---
+++ README.md
@@ -0,0 +1,17 @@
+### Description
+It's a script which verifies transition to kovan-wasm.
+
+### Run
+It's supposed that scripts run from `parity-kovan-wasm-check` dir.
+
+At first make sure you run the script against node with clean database. To clean all data run:
+`cargo run --manifest-path=../parity/Cargo.toml -- db kill --chain spec.json`
+
+Than run parity node:
+`cargo run --manifest-path=../parity/Cargo.toml -- --jsonrpc-apis all --chain spec.json`
+
+And run the script:
+```
+npm install
+node index.js ./identity.wasm
+```
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var Web3 = require("web3");
 var fetch = require("node-fetch-polyfill");
 var fs = require("fs");
 
+var WAIT_FOR = 5000;
+
 var abi = [
     {
       "type": "constructor",
@@ -15,9 +17,17 @@ var abi = [
         var host = "http://localhost:8545";
         var web3 = new Web3(new Web3.providers.HttpProvider(host));
         web3.eth.defaultAccount = "0x004ec07d2329997267ec62b4166639513386f32e";
+        console.log("Creating account " + web3.eth.defaultAccount)
         let res = await rpc(host, "parity_newAccountFromPhrase", ["user", "user"]); // will create 0x004ec07d2329997267ec62b4166639513386f32e
+        if (res != "0x004ec07d2329997267ec62b4166639513386f32e") {
+            throw Error("Creating account failed: parity_newAccountFromPhrase shall return " + web3.eth.defaultAccount);
+        } else {
+            console.log( web3.eth.defaultAccount + " created")
+        }
         if (await web3.eth.personal.unlockAccount(web3.eth.defaultAccount, "user")) {
             console.log("unlock OK");
+        } else {
+            console.log("unlock FAILED");
         }
 
         let codeHex = '0x' + fs.readFileSync(codePath).toString('hex');
@@ -28,17 +38,23 @@ var abi = [
         } catch(e) {
             console.log("First attempt is failed as expected: Wasm isn't enabled on the block #1");
         }
+
+        console.log("Wait for " + WAIT_FOR/1000 + " seconds...");
+        await new Promise((r, e) => setTimeout(() => r(), WAIT_FOR));
+
         Contract = new web3.eth.Contract(abi, { data: codeHex, from: web3.eth.defaultAccount });
-        await new Promise((r, e) => setTimeout(() => r(), 1000));
         if (await web3.eth.personal.unlockAccount(web3.eth.defaultAccount, "user")) {
             console.log("unlock OK");
+        } else {
+            console.log("unlock FAILED");
         }
+
         try {
             console.log("Second attempt to deploy Wasm contract");
-            await Contract.deploy({nonce: 1, data: codeHex, arguments: []}).send({from: web3.eth.defaultAccount, gas: 599433, gasPrice: '100000'});
-            console.log("TEST OK");
+            Contract = await Contract.deploy({nonce: 1, data: codeHex, arguments: []}).send({from: web3.eth.defaultAccount, gas: 599433, gasPrice: '100000'});
+            console.log("TEST OK: Wasm contract was deployed at address: " + Contract.options.address);
         } catch(e) {
-            console.log("TEST FAILED: Wasm should be enabled on the block #2");
+            console.log("TEST FAILED: Wasm should be enabled on the block #2. Check wasmActivationTransition in spec.json");
             console.log(e);
         }
     } catch (e) {
@@ -52,5 +68,5 @@ async function rpc(host, method, params, id = 0) {
         method: 'POST',
         body: JSON.stringify({ jsonrpc:"2.0", method, params, id }),
         headers: {"Content-Type": "application/json"}
-    }).then(res => res.json().result);
+    }).then(res => res.json()).then(json => json.result);
 };
